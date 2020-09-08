7ee63013b6cf5827b459a2c74553c7da4162b2f8
Aman Singh <besoeasy@users.noreply.github.com>
2020-09-08 12:01:14 +0100

    token update

--- src/containers/adressmap/tTokensList.js
+++ src/containers/adressmap/tTokensList.js
@@ -1,147 +1,67 @@
 export const tTokensList = [
-  {
-    name: 'tADA',
-    image: 'https://raw.githubusercontent.com/thirmprotocol/Assets/master/icons/tADA.png',
-    address: ''
-  },
-  {
-    name: 'tBAT',
-    image: 'https://raw.githubusercontent.com/thirmprotocol/Assets/master/icons/tBAT.png',
-    address: ''
-  },
-  {
-    name: 'tBNB',
-    image: 'https://raw.githubusercontent.com/thirmprotocol/Assets/master/icons/tBNB.png',
-    address: ''
-  },
-  {
-    name: 'tBNT',
-    image: 'https://raw.githubusercontent.com/thirmprotocol/Assets/master/icons/tBNT.png',
-    address: ''
-  },
-  {
-    name: 'tBTC',
-    image: 'https://raw.githubusercontent.com/thirmprotocol/Assets/master/icons/tBTC.png',
-    address: ''
-  },
-  {
-    name: 'tCEL',
-    image: 'https://raw.githubusercontent.com/thirmprotocol/Assets/master/icons/tCEL.png',
-    address: ''
-  },
-  {
-    name: 'tCRO',
-    image: 'https://raw.githubusercontent.com/thirmprotocol/Assets/master/icons/tCRO.png',
-    address: ''
-  },
-  {
-    name: 'tDAI',
-    image: 'https://raw.githubusercontent.com/thirmprotocol/Assets/master/icons/tDAI.png',
-    address: ''
-  },
-  {
-    name: 'tDASH',
-    image: 'https://raw.githubusercontent.com/thirmprotocol/Assets/master/icons/tDASH.png',
-    address: ''
-  },
-  {
-    name: 'tDCR',
-    image: 'https://raw.githubusercontent.com/thirmprotocol/Assets/master/icons/tDCR.png',
-    address: ''
-  },
-  {
-    name: 'tEOS',
-    image: 'https://raw.githubusercontent.com/thirmprotocol/Assets/master/icons/tEOS.png',
-    address: ''
-  },
-  {
-    name: 'tETH',
-    image: 'https://raw.githubusercontent.com/thirmprotocol/Assets/master/icons/tETH.png',
-    address: ''
-  },
-  {
-    name: 'tGNO',
-    image: 'https://raw.githubusercontent.com/thirmprotocol/Assets/master/icons/tGNO.png',
-    address: ''
-  },
-  {
-    name: 'tHT',
-    image: 'https://raw.githubusercontent.com/thirmprotocol/Assets/master/icons/tHT.png',
-    address: ''
-  },
-  {
-    name: 'tKNC',
-    image: 'https://raw.githubusercontent.com/thirmprotocol/Assets/master/icons/tKNC.png',
-    address: ''
-  },
-  {
-    name: 'tLEND',
-    image: 'https://raw.githubusercontent.com/thirmprotocol/Assets/master/icons/tLEND.png',
-    address: ''
-  },
-  {
-    name: 'tLINK',
-    image: 'https://raw.githubusercontent.com/thirmprotocol/Assets/master/icons/tLINK.png',
-    address: ''
-  },
-  {
-    name: 'tLRC',
-    image: 'https://raw.githubusercontent.com/thirmprotocol/Assets/master/icons/tLRC.png',
-    address: ''
-  },
-  {
-    name: 'tLTC',
-    image: 'https://raw.githubusercontent.com/thirmprotocol/Assets/master/icons/tLTC.png',
-    address: ''
-  },
-  {
-    name: 'tMKR',
-    image: 'https://raw.githubusercontent.com/thirmprotocol/Assets/master/icons/tMKR.png',
-    address: ''
-  },
-  {
-    name: 'tNANO',
-    image: 'https://raw.githubusercontent.com/thirmprotocol/Assets/master/icons/tNANO.png',
-    address: ''
-  },
-  {
-    name: 'tNEO',
-    image: 'https://raw.githubusercontent.com/thirmprotocol/Assets/master/icons/tNEO.png',
-    address: ''
-  },
-  {
-    name: 'tREN',
-    image: 'https://raw.githubusercontent.com/thirmprotocol/Assets/master/icons/tREN.png',
-    address: ''
-  },
-  {
-    name: 'tREP',
-    image: 'https://raw.githubusercontent.com/thirmprotocol/Assets/master/icons/tREP.png',
-    address: ''
-  },
-  {
-    name: 'tTRX',
-    image: 'https://raw.githubusercontent.com/thirmprotocol/Assets/master/icons/tTRX.png',
-    address: ''
-  },
-  {
-    name: 'tUSDT',
-    image: 'https://raw.githubusercontent.com/thirmprotocol/Assets/master/icons/tUSDT.png',
-    address: ''
-  },
-  {
-    name: 'tWAVES',
-    image: 'https://raw.githubusercontent.com/thirmprotocol/Assets/master/icons/tWAVES.png',
-    address: ''
-  },
-  {
-    name: 'tXLM',
-    image: 'https://raw.githubusercontent.com/thirmprotocol/Assets/master/icons/tXLM.png',
-    address: ''
-  },
-  {
-    name: 'tXMR',
-    image: 'https://raw.githubusercontent.com/thirmprotocol/Assets/master/icons/tXMR.png',
-    address: ''
-  },
+	{
+		name: 'ADA',
+		image: 'https://raw.githubusercontent.com/thirmprotocol/Assets/master/icons/tADA.png',
+		address: '',
+	},
+	{
+		name: 'BNB',
+		image: 'https://raw.githubusercontent.com/thirmprotocol/Assets/master/icons/tBNB.png',
+		address: '',
+	},
+	{
+		name: 'BTC',
+		image: 'https://raw.githubusercontent.com/thirmprotocol/Assets/master/icons/tBTC.png',
+		address: '',
+	},
+	{
+		name: 'DASH',
+		image: 'https://raw.githubusercontent.com/thirmprotocol/Assets/master/icons/tDASH.png',
+		address: '',
+	},
+	{
+		name: 'DCR',
+		image: 'https://raw.githubusercontent.com/thirmprotocol/Assets/master/icons/tDCR.png',
+		address: '',
+	},
+	{
+		name: 'EOS',
+		image: 'https://raw.githubusercontent.com/thirmprotocol/Assets/master/icons/tEOS.png',
+		address: '',
+	},
+	{
+		name: 'LTC',
+		image: 'https://raw.githubusercontent.com/thirmprotocol/Assets/master/icons/tLTC.png',
+		address: '',
+	},
+	{
+		name: 'NANO',
+		image: 'https://raw.githubusercontent.com/thirmprotocol/Assets/master/icons/tNANO.png',
+		address: '',
+	},
+	{
+		name: 'NEO',
+		image: 'https://raw.githubusercontent.com/thirmprotocol/Assets/master/icons/tNEO.png',
+		address: '',
+	},
+	{
+		name: 'TRX',
+		image: 'https://raw.githubusercontent.com/thirmprotocol/Assets/master/icons/tTRX.png',
+		address: '',
+	},
+	{
+		name: 'WAVES',
+		image: 'https://raw.githubusercontent.com/thirmprotocol/Assets/master/icons/tWAVES.png',
+		address: '',
+	},
+	{
+		name: 'XLM',
+		image: 'https://raw.githubusercontent.com/thirmprotocol/Assets/master/icons/tXLM.png',
+		address: '',
+	},
+	{
+		name: 'XMR',
+		image: 'https://raw.githubusercontent.com/thirmprotocol/Assets/master/icons/tXMR.png',
+		address: '',
+	},
 ];
