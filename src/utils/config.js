1dc8f4ed9fda8ea03fa1131b921c872478bdb58a
Aman Singh <besoeasy@users.noreply.github.com>
2020-09-10 21:37:42 +0530

    implemented verified contract

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,10 +1,12 @@
 export const ROPSTEN_URL = 'https://ropsten.infura.io/v3/370da5783b9b4bc086067dcfb45ba7a3';
+export const TOKEN_LIST_URL = 'https://raw.githubusercontent.com/thirmprotocol/Assets/master/data.json';
+export const TOKEN_INTEREST_URL = 'https://raw.githubusercontent.com/thirmprotocol/Assets/master/i.json';
 
 // reference contract
 // https://raw.githubusercontent.com/thirmprotocol/contracts/master/thirmprotocol.sol
 
-export const CONTRACT_ADDRESS = '0xFbDB6A1979A6A429D59994da3df435295D5B264d';
-export const { abi } = require(`./${CONTRACT_ADDRESS}.json`);
+export const CONTRACT_ADDRESS = '0x17136874edBB6Ab273d7cb51FbFaaf4eac812B88';
+export const abi = require(`./${CONTRACT_ADDRESS}.json`);
 
-export const TOKEN_LIST_URL = 'https://raw.githubusercontent.com/thirmprotocol/Assets/master/data.json';
-export const TOKEN_INTEREST_URL = 'https://raw.githubusercontent.com/thirmprotocol/Assets/master/i.json';
+// abi hotlink
+// https://api-ropsten.etherscan.io/api?module=contract&action=getabi&address=${CONTRACT_ADDRESS}&format=raw
