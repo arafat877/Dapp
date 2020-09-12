179ea396026d04ade83e07d80661e517c9259e6e
Aman Singh <besoeasy@users.noreply.github.com>
2020-09-13 02:12:09 +0530

    .

--- src/containers/platform/tPlatformList.js
+++ src/containers/platform/tPlatformList.js
@@ -1,23 +1,22 @@
 const tPlatformList = {
-  "name": "THIRM PROTOCOL",
-  "platforms": [
-    {
-      "Platform": "Bitfinex",
-      "Defination": "Exhcnage based in USA, bla bla "
-    },
-    {
-      "Platform": "bianance",
-      "Defination": "Exhcnage based in Hong Kong, bla bla "
-    },
-    {
-      "Platform": "AAVE",
-      "Defination": "Decenterlised Platform, bla bla "
-    },
-    {
-      "Platform": "Staking",
-      "Defination": "Staking Platform, bla bla "
-    }
-  ]
+	platforms: [
+		{
+			Platform: 'Bitfinex',
+			Defination: 'Exhcnage based in USA, bla bla ',
+		},
+		{
+			Platform: 'Bianance',
+			Defination: 'Exhcnage based in Hong Kong, bla bla ',
+		},
+		{
+			Platform: 'AAVE',
+			Defination: 'Decenterlised Platform, bla bla ',
+		},
+		{
+			Platform: 'Staking',
+			Defination: 'Staking Platform, bla bla ',
+		},
+	],
 };
 
 export default tPlatformList;
