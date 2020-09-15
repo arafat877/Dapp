7a9a5d39be1c3ec7c80d2b3b094a25d298daaa1f
Madhav Poudel <king.ov.universe@gmail.com>
2020-09-14 22:23:17 -0700

    Network rule added

--- src/App.js
+++ src/App.js
@@ -4,13 +4,20 @@ import React from 'react';
 import { RecoilRoot } from 'recoil';
 import Web3 from 'web3';
 import MainContent from './containers';
-import { abi, CONTRACT_ADDRESS, ROPSTEN_URL } from './utils/config';
+import { abi, CONTRACT_ADDRESS_MAINNET, CONTRACT_ADDRESS_ROPSTEN, ROPSTEN_URL } from './utils/config';
 
 function getLibrary(provider) {
 	const library = new Web3Provider(provider);
+
 	const web3 = provider ? new Web3(provider) : new Web3(new Web3.providers.HttpProvider(ROPSTEN_URL));
 	library.web3 = web3;
-	library.contract = new web3.eth.Contract(abi, CONTRACT_ADDRESS);
+	const chainId = provider.chainId;
+	if (chainId === '0x1') {
+		library.contract = new web3.eth.Contract(abi, CONTRACT_ADDRESS_MAINNET);
+	} else if (chainId === '0x3') {
+		library.contract = new web3.eth.Contract(abi, CONTRACT_ADDRESS_ROPSTEN);
+	}
+
 	library.pollingInterval = 8000;
 	return library;
 }
