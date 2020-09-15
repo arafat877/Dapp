0104484a16a8ed6cbe965e252c672d2a9fddc894
Aman Singh <besoeasy@users.noreply.github.com>
2020-09-15 02:38:07 +0100

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -4,8 +4,14 @@ export const TOKEN_LIST_URL = 'https://raw.githubusercontent.com/thirmprotocol/A
 
 export const TOKEN_INTEREST_URL = 'https://raw.githubusercontent.com/thirmprotocol/Assets/master/i.json';
 
+
+
+
 export const CONTRACT_ADDRESS = '0x9B4790D0cd13D3fAFa173ac0F430d832Fdf0144c';
-//export const CONTRACT_ADDRESS = '0x9fd0a63D582cfF5b05AFEaa003E218BAA485ede4';
+
+// if mainet = 0x9B4790D0cd13D3fAFa173ac0F430d832Fdf0144c
+// if ropstern = 0x9fd0a63D582cfF5b05AFEaa003E218BAA485ede4
+// if something else = show error 
 
 export const abi = require(`./abi.json`);
 
