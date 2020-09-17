d319da4105a39a7f77b6a4ab6d4575041dd3334d
Madhav Poudel <king.ov.universe@gmail.com>
2020-09-16 17:46:32 -0700

    Tab added

--- src/containers/mint/index.js
+++ src/containers/mint/index.js
@@ -1,15 +1,28 @@
-import { Col, Row } from 'antd';
+import { Col, Row, Tabs } from 'antd';
 import React from 'react';
 import { MintWrapper } from './style';
 
+const { TabPane } = Tabs;
+
 const Mint = () => {
+
+  function callback(key) {
+    console.log(key);
+  }
 
   return (
     <>
       <Row>
         <Col xs={24}>
           <MintWrapper>
-            <h1>Coming Soon...</h1>
+            <Tabs defaultActiveKey="1" onChange={callback}>
+              <TabPane tab="ERC20" key="1">
+                Content of Tab Pane 1
+    </TabPane>
+              <TabPane tab="Non ERC20" key="2">
+                Content of Tab Pane 2
+    </TabPane>
+            </Tabs>
           </MintWrapper>
         </Col>
       </Row>
--- src/containers/mint/style.js
+++ src/containers/mint/style.js
@@ -1,10 +1,7 @@
 import styled from 'styled-components';
 
 export const MintWrapper = styled.div`
-  text-align: center;
-  margin-top: 120px;
-  font-size: 30px;
-  color: #999;
+  
 `;
 
 
