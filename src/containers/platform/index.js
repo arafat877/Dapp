5c807e9d5f00c3ff397b47fe13dc42b583a878d1
Aman Singh <besoeasy@users.noreply.github.com>
2020-09-20 11:14:11 +0530

    re,oved cdn

--- src/containers/platform/index.js
+++ src/containers/platform/index.js
@@ -1,34 +1,29 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { Card, List } from 'antd';
 import React, { useEffect, useState } from 'react';
-import { PLATFORM_LIST_URL } from '../../utils/config';
 import LoadingIndicator from './../../components/loadingIndicator/index';
 import { TokenCard } from './style';
+const platformsJson = require('../../config/platforms.json');
 
 const { Meta } = Card;
 
 const Platform = () => {
-
 	const [platformsList, setPlatformsList] = useState([]);
 
 	useEffect(() => {
-
-		let stale = false
+		let stale = false;
 
 		const getPlatformsList = async () => {
-			const platformsJson = await fetch(PLATFORM_LIST_URL).then((res) => res.json());
-
 			if (!stale) {
 				setPlatformsList(platformsJson);
 			}
-		}
+		};
 
 		getPlatformsList();
 
 		return () => {
 			stale = true;
 		};
-
 	});
 
 	if (platformsList.length === 0) return <LoadingIndicator />;
--- src/utils/config.js
+++ src/utils/config.js
@@ -1,12 +1,8 @@
-export const TOKEN_LIST_URL = 'https://cdn.thirm.com/tokens.json';
+export const TOKEN_LIST_URL = 'https://thirm.com/tokens.json';
 
 export const TOKEN_INTEREST_URL = 'https://ipfs.eternum.io/ipns/i.thirm.com';
 
 export const LIVE_ETH_PRICE_URL = 'https://api.binance.com/api/v3/ticker/price?symbol=ETHUSDT';
-
-export const PLATFORM_LIST_URL = 'https://cdn.thirm.com/platforms.json';
-
-export const ADDRESSMAP_URL = 'https://cdn.thirm.com/addressmap.json';
 
 export const CONTRACT_ADDRESS_MAINNET = '0x9B4790D0cd13D3fAFa173ac0F430d832Fdf0144c';
 
