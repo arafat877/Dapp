c5a1576827eb8846351af8d280f5cc4701af03f9
Aman Singh <besoeasy@users.noreply.github.com>
2020-09-22 14:44:19 +0530

    fix

--- src/containers/mint/index.js
+++ src/containers/mint/index.js
@@ -1,13 +1,12 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useWeb3React } from '@web3-react/core';
-import { Col, Row, Select, Tabs } from 'antd';
+import { Col, Row, Select } from 'antd';
 import React, { useEffect, useState } from 'react';
 import QRCode from 'react-qr-code';
 import { StyledCard } from '../globalStyle';
 import { MintWrapper } from './style';
 let tempTokenList = require('../../config/addressmap.json');
 
-const { TabPane } = Tabs;
 const { Option } = Select;
 
 const Mint = () => {
@@ -51,31 +50,20 @@ const Mint = () => {
 				<Col xs={24}>
 					<StyledCard>
 						<MintWrapper>
-							<Tabs defaultActiveKey="1" centered>
-								<TabPane tab="ERC20" key="1">
-									<div className="qr-code">
-										<QRCode value="0xA34eF48B73870FA2ACa1558239576cCBC60Ad542" size={200} />
-									</div>
-									<p className="deposite-info">Deposite Address: 0xA34eF48B73870FA2ACa1558239576cCBC60Ad542</p>
-								</TabPane>
-								<TabPane tab="Non ERC20" key="2">
-									<Row>
-										<Col xs={24}>
-											<p>Select Token</p>
-											<p>
-												<Select style={{ width: 200 }} allowClear onChange={onChangeToken} placeholder="Select Coin">
-													{tokensList.map((tkn) => (
-														<Option value={tkn.depositaddress}>{tkn.name}</Option>
-													))}
-												</Select>
-											</p>
+							<Row>
+								<Col xs={24}>
+									<p>
+										<Select style={{ width: 200 }} allowClear onChange={onChangeToken} placeholder="Select Coin">
+											{tokensList.map((tkn) => (
+												<Option value={tkn.depositaddress}>{tkn.name}</Option>
+											))}
+										</Select>
+									</p>
 
-											<div className="qr-code">{selectedToken && <QRCode value={selectedToken} size={200} />}</div>
-											{selectedToken && <p className="deposite-info">Deposite Address: {selectedToken}</p>}
-										</Col>
-									</Row>
-								</TabPane>
-							</Tabs>
+									<div className="qr-code">{selectedToken && <QRCode value={selectedToken} size={200} />}</div>
+									{selectedToken && <p className="deposite-info">Deposit Address : {selectedToken}</p>}
+								</Col>
+							</Row>
 						</MintWrapper>
 					</StyledCard>
 				</Col>
