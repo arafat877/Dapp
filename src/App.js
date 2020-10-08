201db5f3f89a7821e8a25ce28717f1ee756c5db9
Madhav Poudel <king.ov.universe@gmail.com>
2020-10-08 16:20:29 -0700

    fix for loading issue

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ const config = require('./utils/config.json');
 function getLibrary(provider) {
 	const library = new Web3Provider(provider);
 
-	let web3 = new Web3(provider);
+	const web3 = provider ? new Web3(provider) : new Web3(new Web3.providers.HttpProvider(config.RPC_URL));
+
 	library.web3 = web3;
 	library.contract = new web3.eth.Contract(abi, config.CONTRACT_ADDRESS);
 	library.thirm = new web3.eth.Contract(thirmAbi, config.THIRM_CONTRACT_ADDRESS);
