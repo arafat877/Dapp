2a6229047283d4f3bb64b78159acd8270afc7def
Aman Singh <besoeasy@users.noreply.github.com>
2020-10-12 02:43:51 +0100

    added ETH

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import { Web3Provider } from '@ethersproject/providers';
 import { Web3ReactProvider } from '@web3-react/core';
 import React from 'react';
 import { RecoilRoot } from 'recoil';
+import Web3 from 'web3';
 import MainContent from './containers';
 
 function getLibrary(provider) {
 	const library = new Web3Provider(provider);
 	library.pollingInterval = 12000;
+	let web3 = new Web3(provider);
+	library.web3 = web3;
 	return library;
 }
 
